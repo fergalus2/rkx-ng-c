27b3a4b384e01a114a74607296936edf6eb76cde
Luis Fernando Galarcio Marin <luis.galarcio@arkixsa.com>
2018-10-17 02:33:29 -0500

    added filter

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -12,7 +12,19 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart'; //default
+
+  _listFilter: string;  
+  get listFilter(): string {
+    return this._listFilter;
+  }
+    
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  
+
+  filteredProducts: IProduct[];
   products: IProduct[] = [
     {
       "productId": 1,
@@ -36,7 +48,10 @@ export class ProductListComponent implements OnInit {
     }
   ] 
 
-  constructor() { }
+  constructor() { 
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
 
   ngOnInit() {
     console.log('On Init');
@@ -44,5 +59,11 @@ export class ProductListComponent implements OnInit {
 
   toggleImage(): void {
     this.showImage = ! this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 }
